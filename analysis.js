bc87597f5ecc18e62596ec02b41e06495aaa30f8
Andrew Quinlan <andrewquinlan22@gmail.com>
2022-04-16 00:39:55 -0400

    fixed spacing

--- analysis.js
+++ analysis.js
@@ -5,7 +5,7 @@ const fs = require('fs')
 function main () {
   let args = process.argv.slice(2)
 
-  if (args.length == 0) {
+  if (args.length === 0) {
     args = ['analysis.js']
   }
   const filePath = args[0]
@@ -19,14 +19,14 @@ function main () {
   }
 }
 
-var builders = {}
+const builders = {}
 
 // Represent a reusable "class" following the Builder pattern.
 function FunctionBuilder () {
   this.StartLine = 0
   this.FunctionName = ''
   // The number of parameters for functions
-  this.ParameterCount = 0,
+  this.ParameterCount = 0
   // Number of if statements/loops + 1
   this.SimpleCyclomaticComplexity = 1
   // The max depth of scopes (nested ifs, loops, etc)
@@ -36,17 +36,17 @@ function FunctionBuilder () {
 
   this.report = function () {
     console.log(
-		   (
-		   	'{0}(): {1}\n' +
-		   	'============\n' +
-			   'SimpleCyclomaticComplexity: {2}\t' +
-				'MaxNestingDepth: {3}\t' +
-				'MaxConditions: {4}\t' +
-				'Parameters: {5}\n\n'
+	  (
+        '{0}(): {1}\n' +
+		'============\n' +
+		'SimpleCyclomaticComplexity: {2}\t' +
+		'MaxNestingDepth: {3}\t' +
+		'MaxConditions: {4}\t' +
+		'Parameters: {5}\n\n'
       )
         .format(this.FunctionName, this.StartLine,
-				     this.SimpleCyclomaticComplexity, this.MaxNestingDepth,
-			        this.MaxConditions, this.ParameterCount)
+			  this.SimpleCyclomaticComplexity, this.MaxNestingDepth,
+			  this.MaxConditions, this.ParameterCount)
     )
   }
 };
@@ -62,9 +62,9 @@ function FileBuilder () {
   this.report = function () {
     console.log(
       ('{0}\n' +
-			  '~~~~~~~~~~~~\n' +
-			  'ImportCount {1}\t' +
-			  'Strings {2}\n'
+	   '~~~~~~~~~~~~\n' +
+	   'ImportCount {1}\t' +
+	   'Strings {2}\n'
       ).format(this.FileName, this.ImportCount, this.Strings))
   }
 }
@@ -80,7 +80,7 @@ function traverseWithParents (object, visitor) {
     if (object.hasOwnProperty(key)) {
       child = object[key]
       if (typeof child === 'object' && child !== null && key != 'parent') {
-            	child.parent = object
+        child.parent = object
         traverseWithParents(child, visitor)
       }
     }
@@ -158,7 +158,7 @@ function max (a, b) {
 // Helper function for checking if a node is a "decision type node"
 function isDecision (node) {
   if (node.type == 'IfStatement' || node.type == 'ForStatement' || node.type == 'WhileStatement' ||
-		 node.type == 'ForInStatement' || node.type == 'DoWhileStatement') {
+	  node.type == 'ForInStatement' || node.type == 'DoWhileStatement') {
     return true
   }
   return false
@@ -254,7 +254,7 @@ remainder.toString() + ' seconds'
 				  return
     }
     return hours.toString().split('.')[0] + ' ' + szhr + ' ' +
-mints.toString().split('.')[0] + ' ' + szmin
+		   mints.toString().split('.')[0] + ' ' + szmin
   }
 }
 exports.complexity = complexity
